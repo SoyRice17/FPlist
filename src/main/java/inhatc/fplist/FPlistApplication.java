43e9345c36a744c138588c03bfe9b6d6a7a702f4
SoyRice17 <rkswkdqkq6974@gmail.com>
2025-06-20 22:13:15 +0900

    project create

---
+++ src/main/java/inhatc/fplist/FPlistApplication.java
@@ -0,0 +1,13 @@
+package inhatc.fplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FPlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FPlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inhatc/fplist/FPlistApplicationTests.java
@@ -0,0 +1,13 @@
+package inhatc.fplist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FPlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
